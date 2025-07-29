c533a76513cb8a299096fa7035a062098a889cfd
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-07-29 22:32:48 +0600

    fix: eslint in coupon service

--- src/main/coupon/coupon.service.ts
+++ src/main/coupon/coupon.service.ts
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { Injectable } from '@nestjs/common';
 import { CreateCouponDto } from './dto/create-coupon.dto';
 import { UpdateCouponDto } from './dto/update-coupon.dto';
