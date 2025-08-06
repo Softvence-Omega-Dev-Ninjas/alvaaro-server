79186af02d94d9aaee92f55d91cc64d2a459f0d4
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-08-07 01:57:42 +0600

    tiktok video uploaded successfully

--- src/main/socials/tiktok/dto/create-tiktok.dto.ts
+++ src/main/socials/tiktok/dto/create-tiktok.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTiktokDto {}
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UploadTiktokVideoDto {
+  @ApiProperty({
+    type: 'string',
+    description: 'Title of the TikTok video',
+    example: 'My Awesome TikTok Video',
+  })
+  title: string;
+
+  @ApiProperty({
+    type: 'string',
+    format: 'binary',
+    description: 'MP4 video file to upload',
+  })
+  video: any;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'TikTok Access Token',
+    example:
+      'act.vijtqLqn7Jlgy4C13y1A3uVvQpOHVbrGE0LnpZaUrFtrBpicDTydbqEyMcj5!4450.va',
+  })
+  accessToken: string;
+}
