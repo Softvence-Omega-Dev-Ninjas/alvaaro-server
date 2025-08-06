3b09a663597ba82eef1a52d8fff373d8280ca601
Mahammad Shariar Alam <mahammadshariaralam@gmail.com>
2025-08-07 00:45:23 +0600

    tiktok auth service implemented successfully

--- src/main/socials/tiktok/controller/tiktok-auth.controller.ts
+++ src/main/socials/tiktok/controller/tiktok-auth.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { TiktokAuthService } from '../services/tiktok-auth.service';
 
 @Controller('tiktok-auth')
-export class TiktokAuthController {}
+export class TiktokAuthController {
+  constructor(private readonly tiktokAuthService: TiktokAuthService) {}
+  // Define your methods for handling TikTok authentication here
+  @Get('callback')
+  async handleCallback(@Query('code') code: string, @Res() res: Response) {
+    try {
+      const tokenResponse = (await this.tiktokAuthService.getAccessToken(
+        code,
+      )) as {
+        access_token: string;
+        refresh_token: string;
+        [key: string]: any;
+      };
+      //   console.log({ tokenResponse });
+      //   const redirectUrl = `http://localhost:3001/?accessToken=${tokenResponse.access_token}&refreshToken=${tokenResponse.refresh_token}`;
+      //   return res.redirect(redirectUrl);
+      //   console.log(`Redirecting to: ${redirectUrl}`);
+      res.send({
+        accessToken: tokenResponse.access_token,
+        refreshToken: tokenResponse.refresh_token,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
--- src/main/socials/tiktok/tiktok.module.ts
+++ src/main/socials/tiktok/tiktok.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TiktokAuthService } from './services/tiktok-auth.service';
 import { TiktokAuthController } from './controller/tiktok-auth.controller';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [TiktokAuthController],
   providers: [TiktokAuthService, TiktokAuthService],
 })
