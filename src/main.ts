048c8975063e4991c549bf399d6c175257fe0084
azmain-siam <siamhossain2700@gmail.com>
2025-09-25 11:24:56 +0600

    fix: cors issue

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,13 @@ import { seed } from './prisma-service/seed';
 async function bootstrap() {
   // apply seed data
   await seed();
+
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalFilters(new AllExceptionsFilter());
-  app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'http://localhost:5174',
-      'https://xn--privestate-e7a.com',
-    ],
-    credentials: true,
-  });
+
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('Alvaaro Server')
     .setDescription('API description')
