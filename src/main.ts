7cc991cb10723d58d2180984e8944cab06ed783c
Imran Hossain <81389032+imranwebstudio@users.noreply.github.com>
2025-10-06 14:18:31 +0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ async function bootstrap() {
       'http://localhost:5173',
       'http://localhost:5174',
       'https://xn--privestate-e7a.com',
+      'https://xn--privestate-e7a.es',
     ],
     credentials: true,
   });
