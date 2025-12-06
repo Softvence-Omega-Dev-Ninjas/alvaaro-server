77bc2a72aede51f1c7ecff54d857a73f1c925c19
mahammadshariaralamsarker <mahammadshariaralam@gmail.com>
2025-12-07 00:25:46 +0600

    feat: Enhance server startup message with base URL
    Adds the base URL (http://localhost) to the server startup message in
    `main.ts` for a more complete URL when accessing the API.

--- src/main.ts
+++ src/main.ts
@@ -59,7 +59,7 @@ async function bootstrap() {
 
   app.use('/uploads', serverFile(join(process.cwd(), 'public', 'uploads')));
   await app.listen(process.env.PORT ?? 3000, () => {
-    console.log(`⚡Server is running on port: ${process.env.PORT ?? 3000}`);
+    console.log(`⚡Server is running on port: http://localhost:${process.env.PORT ?? 3000}/api`);
   });
 }
 void bootstrap();
